633a25b76d54233fcb1118b5a5b8cb3dc2c4d8b0
yoon.homme <woosiks.io@gmail.com>
2018-12-06 19:49:20 +0900

    Add files

--- ch7/7.6/learn-sequenlize/routes/index.js
+++ ch7/7.6/learn-sequenlize/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var User = require('../models').User;
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  User.findAll()
+  .then((users)=>{
+    res.render('sequenlize', {users});
+  })
+  .catch((err)=>{
+    console.error(err);
+    next(err);
+  });
+  // res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
